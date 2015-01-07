f333029e6ec154ca01726428198cf847e748efdc
Andrew Chen <andrewchen5678@gmail.com>
2015-01-06 22:53:35 -0800

    testing universal

--- app/src/main/java/com/itdyn/andrewapp/MainActivity.java
+++ app/src/main/java/com/itdyn/andrewapp/MainActivity.java
@@ -6,13 +6,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import org.xwalk.core.XWalkView;
+
 
 public class MainActivity extends Activity {
+
+    private XWalkView mXWalkView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mXWalkView = (XWalkView) findViewById(R.id.xwalkView);
+        //mXWalkView.
+        mXWalkView.load("http://crosswalk-project.org/", null);
     }
 
 

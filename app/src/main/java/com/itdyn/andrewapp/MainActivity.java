da950e99b264faeaae6f9d3b90fe111e1d91a774
Andrew Chen <andrewchen5678@gmail.com>
2015-01-09 22:29:20 -0800

    prevent memory leak

--- app/src/main/java/com/itdyn/andrewapp/MainActivity.java
+++ app/src/main/java/com/itdyn/andrewapp/MainActivity.java
@@ -2,39 +2,64 @@ package com.itdyn.andrewapp;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebResourceResponse;
 
+import org.xwalk.core.XWalkResourceClient;
+import org.xwalk.core.XWalkUIClient;
 import org.xwalk.core.XWalkView;
 
 
 public class MainActivity extends Activity {
 
-    private XWalkView mXWalkView;
+    private XWalkView mXwalkView;
+
+    class MyResourceClient extends XWalkResourceClient {
+        MyResourceClient(XWalkView view) {
+            super(view);
+        }
+
+        @Override
+        public WebResourceResponse shouldInterceptLoadRequest(XWalkView view, String url) {
+         return super.shouldInterceptLoadRequest(view,url);
+        }
+    }
+
+    class MyUIClient extends XWalkUIClient {
+        MyUIClient(XWalkView view) {
+            super(view);
+        }
+
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mXWalkView = (XWalkView) findViewById(R.id.xwalkView);
-        //mXWalkView.
-
-        if (savedInstanceState == null){
-            mXWalkView.load("http://crosswalk-project.org/", null);
-        }
+        mXwalkView = (XWalkView) findViewById(R.id.xwalkView);
+        //mXwalkView.
+        mXwalkView.setResourceClient(new MyResourceClient(mXwalkView));
+        mXwalkView.setUIClient(new MyUIClient(mXwalkView));
+        //if (savedInstanceState == null){
+            mXwalkView.load("http://crosswalk-project.org/", null);
+        //}
     }
 
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-        mXWalkView.restoreState(savedInstanceState);
+        //mXwalkView.restoreState(savedInstanceState);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mXWalkView.saveState(outState);
+        //mXwalkView.saveState(outState);
 
     }
 
@@ -58,5 +83,45 @@ public class MainActivity extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mXwalkView != null) {
+            mXwalkView.pauseTimers();
+            mXwalkView.onHide();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (mXwalkView != null) {
+            mXwalkView.resumeTimers();
+            mXwalkView.onShow();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mXwalkView != null) {
+            mXwalkView.onDestroy();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (mXwalkView != null) {
+            mXwalkView.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        if (mXwalkView != null) {
+            mXwalkView.onNewIntent(intent);
+        }
     }
 }
